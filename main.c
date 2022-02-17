930af25232812014e91ba948f579364b834e3dc7
Janette Arrakis <jarrakis@il-a1.msk.21-school.ru>
2022-02-17 21:04:13 +0300

    started

---
+++ main.c
@@ -0,0 +1,12 @@
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		print_error();
+		exit(0);
+	}
+	printf("OK\n");
+}
---
+++ push_swap.h
@@ -0,0 +1,11 @@
+
+#ifndef PUSH_SWAP_PUSH_SWAP_H
+# define PUSH_SWAP_PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h> // delite
+
+void	print_error(void);
+
+#endif
